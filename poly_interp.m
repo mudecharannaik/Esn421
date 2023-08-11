d76e504c0b95a5e989d37d4c2ee3108a374671b7
mudecharannaik <97876246+mudecharannaik@users.noreply.github.com>
2023-08-11 16:29:08 +0530

    Update poly_interp.m

--- poly_interp.m
+++ poly_interp.m
@@ -1,13 +1,35 @@
-N = 5;
-x = linspace(1,10,N); 
-y = randi(10,[1,N]);
+N= 5;
+x= linspace(1,10,N);
+y=randi(10,[1,N]);
+y = y.';
+% prepare G matrix  - Dimension (N+1) x (N+1)
+%    [x0^0  x0^1  x1^2 ..... x1^N]
+%    |x1^0  x1^1  x1^2 ..... x1^N|
+%    |x2^0  x2^1  x2^2 ..... x2^N|
+%G = |x3^0  x3^1  x3^2 ..... x3^N|
+%    |:  :   :     :     :    :  |
+%    [xN^0  xN^1  xN^2 ..... xN^N]
+G=zeros(N,N);
+for i = 1:N
+    for j = 1:N
+        G(i,j) = x(i)^(j-1);
+    end
+end
+m = inv(G) * y;
 
-x1 = [x.^0; x.^1; x.^2; x.^3; x.^4]
-x1T = x1.'% x1 transpose
-G = x1T % G = x1_transpose
-% d = G m;
+xi = linspace(x(1),x(end),1000);
 
-d = y
-d_I = y.'
-G_I = inv(G)
-m = G_I * d_I
+xi = xi.';
+yi = zeros(1000,1);
+for j=1:N
+    yi = yi+m(j)*(xi.^(j-1))
+end
+plot(xi,yi)
+hold on
+plot(x,y,'o','markerFacecolor','y')
+xlabel('X')
+ylabel('Y')
+title('Polynomial Interpolation')
+
+
+
